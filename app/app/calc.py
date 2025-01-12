b1e64608d06ba8ba33413efbf9c2a0d91f865a6b
Saini-Aayush_bcgprod <saini.aayush@bcg.com>
2025-01-12 23:31:11 +0530

    added sample tests and added db config in docker compose and dockerfile

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator function
+"""
+
+def add(x, y):
+    """ Adds x & y, returns the sum"""
+    return x + y
+
+def subtract(x, y):
+    """ Subtracts y from x"""
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+from . import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """ Test adding numbers together"""
+        res = calc.add(5, 6)
+        self.assertEquals(res, 11)
+
+    def test_subtract_numbers(self):
+        """ Test subtracting the numbers"""
+        res = calc.subtract(6, 5)
+        self.assertEquals(res, 1)
